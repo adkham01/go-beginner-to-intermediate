49d698533551afea2b67223d0ddcfee1d523c699
adhambek010 <adhamsalaydinov5@gmail.com>
2025-11-06 01:11:11 +0100

    Begining with go

---
+++ lesson1/first.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"math/cmplx"
+)
+
+func swap(x string, y string) (string, string) {
+	return y, x
+}
+
+func split(sum int) (x, y int) {
+	x = sum * 4 / 8
+	y = sum - x
+	return
+}
+
+var (
+	toBe   bool       = false
+	maxInt uint64     = 1<<64 - 1
+	z      complex128 = cmplx.Sqrt(-5 + 12i)
+)
+
+func main() {
+	var a, b string = "world", "Hello"
+	fmt.Println(swap(a, b))
+	fmt.Println(split(17))
+
+	fmt.Printf("type: %T Value %v\n", toBe, toBe)
+	fmt.Printf("type: %T Value %v\n", maxInt, maxInt)
+	fmt.Printf("type: %T Value %v\n", z, z)
+
+	var i int
+	var f float64
+	var db bool
+	var s string
+	fmt.Printf("%v, %v, %v, %q\n", i, f, db, s)
+}
